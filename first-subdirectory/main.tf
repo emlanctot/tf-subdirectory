569163a4f847c6dc1c1a6b85255857adec654dc8
emlanctot <emmalanctot@gmail.com>
2024-09-11 15:21:59 -0700

    change for prod test

--- first-subdirectory/main.tf
+++ first-subdirectory/main.tf
@@ -18,5 +18,3 @@ resource "null_resource" "trois" {}
 resource "null_resource" "cat" {}
 resource "null_resource" "sank" {}
 resource "null_resource" "sheesh" {}
-resource "null_resource" "beep" {}
-resource "null_resource" "boop" {}
